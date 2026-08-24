c7da09db9fc9507aa711cf0ead0948d50865b48b
Lukas Erlacher <lerlacher@fastmailteam.com>
2026-08-24 13:49:57 +1000

    fix typo

--- lib/Synergy/Reactor/PagerDuty.pm
+++ lib/Synergy/Reactor/PagerDuty.pm
@@ -580,7 +580,7 @@ command resolve => {
     });
   }
 
-  return await $event->error_reply("I don't know what you want to ack.  Check the help!");
+  return await $event->error_reply("I don't know what you want to resolve.  Check the help!");
 };
 
 command snooze => {
